6ad10d16ffaa3034a8ca8075e8ac9b874c52ae99
narensrinivasan <85373920+narensrinivasan@users.noreply.github.com>
2021-08-10 23:11:05 -0700

    Fixed the arrangement of blocks in rooms

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ from pygame.locals import *
 class Builder:
 
 
-    def __init__(self, SCREEN_WIDTH = 1200, SCREEN_HEIGHT = 800, NUM_ROOMS = 1, BLOCK_SIZE = 100):
+    def __init__(self, SCREEN_WIDTH = 1200, SCREEN_HEIGHT = 800, NUM_ROOMS = 1, BLOCK_SIZE = 50):
         self.rooms = []
+        self.blockSize = BLOCK_SIZE
         for i in range(NUM_ROOMS):
             self.rooms.append([])
             for x in range(0,SCREEN_HEIGHT, BLOCK_SIZE):
                 for y in range(0,SCREEN_WIDTH, BLOCK_SIZE):
-                    self.rooms[i].append(Block(x,y,(0,255,0)))
-            print(len(self.rooms[0]))
+                    self.rooms[i].append(Block(x,y,"C:/Users/nitna/OneDrive/Documents/GitHub/ClairDOmbre/floor_wall_blocks.png"))
         self.roomNum = 0
             
     #Receives Room rectangle codes, then changes corresponding blocks into background blocks accordingly
     # YEAH MATH!!! (I am not good at math)
-    # ROOMS DEFINED BY : (x Pos, y Pos, y Height, x Height) confusing, I know.
-    def buildNewRoom(self, rooms = [(1,1,4,8),(3,4,3,5)]) :
-        width = (int) (SCREEN_WIDTH/100)
+    # ROOMS DEFINED BY : (x Pos, y Pos, Height, Width) confusing, I know.
+    def buildNewRoom(self, rooms = [(1,8,6,15),(7,5,3,6)]) :
+        width = (int) (SCREEN_WIDTH/self.blockSize)
         for i in rooms:
             index = i[0]
             index += (i[1]*width)
             num = i[2] * i[3]
             for j in range(index,index+(width*i[2]), width) :
                 for k in range(i[3]) :
-                    self.rooms[0][j+k].setColor((255,0,0))
+                    self.rooms[0][j+k].setSprite("C:/Users/nitna/OneDrive/Documents/GitHub/ClairDOmbre/background_tile.png")
             
     
 
@@ -39,17 +39,14 @@ class Block(pygame.sprite.Sprite):
         self.posX = posX
         self.posY = posY
         super(Block, self).__init__()
-        self.surface = pygame.Surface((100,100))
-        self.surface.fill(color)
-        self.rect = self.surface.get_rect()
+        self.rect = pygame.Rect(posX,posY,50,50)
+        self.image = pygame.image.load(color)
 
     def getPos(self):
         return (self.posY,self.posX)
 
-    def setColor(self, color) :
-        if isinstance(color, tuple):
-            if len(color) == 3 :
-                self.surface.fill(color)
+    def setSprite(self, sprite = "C:/Users/nitna/OneDrive/Documents/GitHub/ClairDOmbre/floor_wall_blocks.png"):
+        self.image = pygame.image.load(sprite)
                 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -57,6 +54,8 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((50, 50))
         self.surf.fill((0,0,0))
         self.rect = self.surf.get_rect()
+
+#setup
 
 pygame.init()
 
@@ -67,12 +66,14 @@ screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 screen.fill((255,255,255))
 
 player = Player()
-builder = Builder()
+builder = Builder(SCREEN_WIDTH, SCREEN_WIDTH, 1, 50)
 
+#builds the default room (currently)
 builder.buildNewRoom()
 
+#paints all the blocks, background or nah
 for block in builder.yieldBlocks() :
-    screen.blit(block.surface, block.getPos())
+    screen.blit(block.image, block.getPos())
 
 
 screen.blit(player.surf, (100,100))
